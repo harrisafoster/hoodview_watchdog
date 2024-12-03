b1e1f10f5f3b97f88117384900b93f0fbc0eeaff
zeherminator <zeherminator@BigDickDebian.bigdickdebian.com>
2024-12-03 21:54:34 +0100

    Chromedriver set up moved out of loop

--- src/main.rs
+++ src/main.rs
@@ -28,13 +28,15 @@ async fn main() {
     log4rs::init_file("log4rs.yml", Default::default()).unwrap();
     log::info!("Starting main thread...");
 
+    // Set up chromedriver
+    let chromedriver: ChromeDriver = ChromeDriver::spawn().unwrap();
+    let session_url: String = chromedriver.url().to_owned();
+    let mut caps: thirtyfour::ChromeCapabilities = DesiredCapabilities::chrome();
+    caps.add_arg("--headless=new").unwrap();
+    caps.add_arg("--start-maximized").unwrap();
+    let driver: WebDriver = WebDriver::new(session_url, caps).await.unwrap();
+
     loop {
-        let chromedriver: ChromeDriver = ChromeDriver::spawn().unwrap();
-        let session_url: String = chromedriver.url().to_owned();
-        let mut caps: thirtyfour::ChromeCapabilities = DesiredCapabilities::chrome();
-        caps.add_arg("--headless=new").unwrap();
-        caps.add_arg("--start-maximized").unwrap();
-        let driver: WebDriver = WebDriver::new(session_url, caps).await.unwrap();
         let available_dates: Vec<(i32, Vec<CalendarAvailabilityDate>)> = scrape_all(
             &driver
         ).await.unwrap();
@@ -47,7 +49,6 @@ async fn main() {
         } else {
             log::warn!("No availability found :(");
         }
-        driver.quit().await.unwrap();
 
         let sleep_interval: time::Duration = time::Duration::from_secs(converted_interval);
         log::warn!("Sleeping for {} seconds", converted_interval);
